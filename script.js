e115db8f225adeea79e13def3083d1610742e3df
DavidBcyber <dbustos28@amarillocollege.com>
2025-03-17 17:00:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+    // display alert good after
+    window.alert("Good afternoon")
+    // declare the variable to hold the div element
+    let container = document.getElementById("container");
+    //set the content of element
+    container.innerHTML = "This is the container place";
+
+    container.style.color = "red";
+
+    document.write("<p>This is new paragraph</p>");
